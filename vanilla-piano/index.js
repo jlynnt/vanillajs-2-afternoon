2ff3f1b728b4503844741d069dcf5e5c1131e120
t'ana <tana@MacBooks-MacBook-Pro.local>
2020-02-20 08:20:26 -0700

    first commit

---
+++ vanilla-piano/index.js
@@ -0,0 +1,15 @@
+window.addEventListener("keypress", function(keyPressEvent){
+ const tone = document.querySelector(`audio[data-key="${keyPressEvent.keyCode}"]`);
+      if(!tone) return;
+ const pianoKey = document.querySelector(`pianoKey[data-key="${keyPressEvent.keyCode}"]`);
+ tone.currentTime = 0;
+
+ tone.play();
+ setTimeout(function() {
+     pianoKey.classList.remove("pressed"); 
+ }, 300);
+ });
+
+
+      
+
